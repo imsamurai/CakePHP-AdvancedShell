00ccc66478af41bde62b9db3660309a2a762d77f
imsamurai <imsamurai@imsamurai-2>
2014-04-03 10:59:05 +0300

    fix test

--- Test/Case/Console/Command/AdvancedShellTest.php
+++ Test/Case/Console/Command/AdvancedShellTest.php
@@ -83,7 +83,7 @@ class AdvancedShellTest extends CakeTestCase {
 
 		$this->assertSame($dumpVisible, !empty($this->out));
 		if ($dumpVisible) {
-			$this->assertRegExp('/SELECT .* \<sqlinfo\>{e:, a:0, t:0, n:0}\<\/sqlinfo\>/i', $this->out);
+			$this->assertRegExp('/SELECT .* \<sqlinfo\>{e:, a:0, t:\d+, n:0}\<\/sqlinfo\>/i', $this->out);
 		}
 	}
 
